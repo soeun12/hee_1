5f99685322aca6f216e9d44a39cfadddf35ceaed
soeune12 <soeunee_@naver.com>
2022-06-16 23:43:32 +0900

    3번 문제의 답안

--- as/app.R
+++ as/app.R
@@ -1,49 +1,69 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(flexdashboard)
 library(shiny)
+library(shinyWidgets)
+library(ggplot2)
+library(magrittr)
+library(plotly)
+library(dplyr)
+library(palmerpenguins)
+library(DT)
+library(stringr)
+library(shinythemes)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui = fluidPage(theme = shinytheme("superhero"),
+               titlePanel(
+                 "펭귄 데이터 분석"
+               ),
+               
+               sidebarLayout(
+                 sidebarPanel( 
+                   
+                   checkboxGroupInput("species", label = h4("펭귄 종류를 선택하세요"),
+                                      choices = list("Adelie", "Gentoo", "Chinstrap"), 
+                                      selected = 1),
+                   
+                   selectInput("x", label = h3("x축을 선택하세요."), 
+                               choices = list("bill_length_mm","bill_depth_mm" , "flipper_length_mm" , "body_mass_g"), 
+                               selected = 'bill_depth_mm'),
+                   
+                   selectInput("y", label = h3("y축을 선택하세요."), 
+                               choices = list("bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"), 
+                               selected = 'bill_length_mm'),
+                   
+                   
+                   sliderInput(inputId='size',
+                               label="점 크기를 선택하세요",
+                               min = 1, max =10, value =5)
+                   
+                   
+                 ),
+                 
+                 mainPanel(
+                   dataTableOutput('p_table'),
+                   plotOutput('p_plot')
+                 )
+               )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server =function(input,output,session) {
+  
+  sel_p = reactive({
+    penguins %>%
+      select(species, island, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, sex, year) %>%
+      filter(species %in% input$species)
+  })
+  
+  
+  output$p_table = renderDataTable({
+    sel_p() %>%
+      datatable()
+  })
+  
+  output$p_plot = renderPlot({
+    penguins %>%
+      ggplot(aes_string(x= input$x, y=input$y)) +
+      geom_point(aes(color=species, shape=sex),size=as.numeric(input$size))+
+      theme_bw()
+  })}
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
